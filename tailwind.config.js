eebdf5800c4f71fd382f0c82afd1254010cfbc35
Khalidprithy <khalidprithy@gmail.com>
2022-05-22 19:17:29 +0600

    daizyUI added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        toolSegytheme: {
+          primary: "#484c7f",
+          secondary: "#f5558d",
+          accent: "#9cbf3b",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
